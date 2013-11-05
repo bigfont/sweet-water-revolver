736aa068ccbc58ca4a38221be9514c641f9fa754
Shaun Luttin <shaunluttin@gmail.com>
2013-11-05 13:55:54 -0800

    Make domain a property for reuse.

--- mvcWebApp/Controllers/HomeController.cs
+++ mvcWebApp/Controllers/HomeController.cs
@@ -10,20 +10,32 @@ namespace mvcWebApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        public string domain
+        {
+            get
+            {
+                string domain =
+                    Request.Url.Scheme +
+                    System.Uri.SchemeDelimiter +
+                    Request.Url.Host +
+                    (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+
+                return domain;
+            }
+        }
+
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            string domain = 
-                Request.Url.Scheme + 
-                System.Uri.SchemeDelimiter + 
-                Request.Url.Host +
-                (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+
 
             // NICE-TO-HAVE Sort images by height.
             string imagesDir = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "Images");
-            string[] files = Directory.EnumerateFiles(imagesDir).Select(p => domain + "/Images/" + Path.GetFileName(p)).ToArray();
+            string[] files = Directory.EnumerateFiles(imagesDir).Select(p => this.domain + "/Images/" + Path.GetFileName(p)).ToArray();
 
             ViewBag.ImageVirtualPaths = files;
 
